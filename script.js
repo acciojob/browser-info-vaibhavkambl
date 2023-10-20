51442b59b0d4ea90362375421e976330f4412517
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-10-20 17:34:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+        const browserInfoDiv = document.getElementById('browser-info');
+        const browserName = navigator.appName;
+        const version = navigator.appVersion;
+
+        browserInfoDiv.innerText = "You are using " + browserName + " version " + version;
+    });
